d509618be6ac7015a4667c0d08e6bf904e52f610
Qinxin Zhang <qinxin.zhang@lotusflare.com>
2021-12-22 16:36:20 +0800

    first commit

--- app/src/main/java/com/example/composeapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeapp/ui/theme/Theme.kt
@@ -37,7 +37,7 @@ fun ComposeAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Compos
 
     MaterialTheme(
         colors = colors,
-        typography = Typography,
+        typography = PopinsTypography,
         shapes = Shapes,
         content = content
     )
--- app/src/main/java/com/example/composeapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/composeapp/ui/theme/Type.kt
@@ -2,16 +2,56 @@ package com.example.composeapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.composeapp.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+
+val Popins = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.W400),
+    Font(R.font.poppins_medium, FontWeight.W500)
+)
+
+val PopinsTypography = Typography(
+    h1 = TextStyle(
+        fontFamily = Popins,
+        fontWeight = FontWeight.W400,
+        fontSize = 32.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = Popins,
+        fontWeight = FontWeight.W400,
+        fontSize = 22.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = Popins,
+        fontWeight = FontWeight.W400,
+        fontSize = 18.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = Popins,
+        fontWeight = FontWeight.W400,
         fontSize = 16.sp
+    ),
+
+    h5 = TextStyle(
+        fontFamily = Popins,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp
+    ),
+
+    h6 = TextStyle(
+        fontFamily = Popins,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Popins,
+        fontWeight = FontWeight.W400,
+        fontSize = 10.sp
     )
     /* Other default text styles to override
     button = TextStyle(
@@ -25,4 +65,5 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+
 )
